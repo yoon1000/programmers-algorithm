f8776b054da3e6e6fe1a55dcb58c709fe696890d
yoon1000 <lucayang@naver.com>
2021-07-26 02:59:14 +0900

    Update keypad.java/ Not Solved

--- Level1/keypad.java
+++ Level1/keypad.java
@@ -1,27 +1,61 @@
 //level 1 - 키패드 누르기
-import java.util.HashMap;
+import java.util.Stack;
+
 class Solution {
     public String solution(int[] numbers, String hand) {
-        String answer = "";
-        int L[] = {1,4,7};
-        int R[] = {3,6,9};
-        HashMap<Integer, String> hashMap = new HashMap<>();
-        for(int i = 0;i < 10; i++){
-            hashMap.put(i , "X");
-        }
-        hashMap.put(10,"L");
-        hashMap.put(11,"R");
-        for(int i = 0; i < numbers.length; i++){
-            if(numbers[i]==1 || numbers[i]==4 || numbers[i]==7){
+       String answer = "";
+        Stack<Integer> Lstack = new Stack<>();
+        Stack<Integer> Rstack = new Stack<>();
+        Lstack.push(10);
+        Rstack.push(12);
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7) {
                 answer += "L";
+                Lstack.pop();
+                Lstack.push(numbers[i]);
             }
-            else if(numbers[i]==3 || numbers[i]==6 || numbers[i]==9){
+            else if (numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9) {
                 answer += "R";
+                Rstack.pop();
+                Rstack.push(numbers[i]);
             }
-            else if(numbers[i]==2 || numbers[i]==5 || numbers[i]==8){
-                answer += "R";
+            else {
+                if(calculate(Lstack.peek(), numbers[i]) < calculate(Rstack.peek(), numbers[i])){
+                    answer += "L";
+                    Lstack.pop();
+                    Lstack.push(numbers[i]);
+                }
+                else if(calculate(Lstack.peek(), numbers[i]) > calculate(Rstack.peek(), numbers[i])){
+                    answer += "R";
+                    Rstack.pop();
+                    Rstack.push(numbers[i]);
+                }
+                else{
+                    if(hand == "left"){
+                        answer += "L";
+                        Lstack.pop();
+                        Lstack.push(numbers[i]);
+                    }
+                    else{
+                        answer += "R";
+                        Rstack.pop();
+                        Rstack.push(numbers[i]);
+                    }
+                }
             }
         }
         return answer;
+    }
+    
+    public static int calculate(int peek, int input){
+        if(input == 0 )
+            input = 11;
+        if(peek == 0)
+            peek = 11;
+        int Peek[] = {(peek-1)/3, (peek-1)%3};
+        int Input[] = {(input-1)/3, (input-1)%3};
+        int length = Math.abs(Input[0] - Peek[0]) + Math.abs(Input[1] - Peek[1]);
+        return length;
     }
 }
