5a75df98750b3339356acd2b5a53b6322a089017
yoon1000 <lucayang@naver.com>
2021-08-14 22:46:21 +0900

    Weekly Challenge 2주차 - 상호 평가

--- weekly-challenge/grading.java
+++ weekly-challenge/grading.java
@@ -1,62 +1,43 @@
 //weekly challenge 1주차 - 부족한 금액 계산하기
 
-import java.util.ArrayList;
-
 class Solution {
     public String solution(int[][] scores) {
-        String answer = "";
+       String answer = "";
         int size = scores.length;
         int max = 0;
-        int min = 0;
+        int min = 100;
         float average[] = new float[size];
-
-        //list 안의 list를 이용해서 각자 평가받은 점수를 넣어준다.
-        ArrayList<ArrayList<Integer>> arrayList = new ArrayList<>();
-        for(int i = 0; i < size; i++){
-            arrayList.add(new ArrayList<Integer>());
-        }
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                arrayList.get(i).add(scores[j][i]);
-            }
-            arrayList.get(i).sort(null);
-        }
-
-        //myscore[]에 자기자신이 평가한 점수를 넣어준다.
         int myscore[] = new int[size];
+        boolean flag = false;
+        
         for(int i = 0; i < size; i++){
             myscore[i] = scores[i][i];
+            for(int j = 0; j < size; j++){
+                average[i] += scores[j][i];
+                if (max < scores[j][i])
+                    max = scores[j][i];
+                if (min > scores[j][i])
+                    min = scores[j][i];
+                if(i != j && myscore[i] == scores[j][i])
+                    flag = true;
+            }
+
+            if(flag && (max == myscore[i] || min == myscore[i])){
+                average[i] = average[i]/size;
+            }
+            else if(max == myscore[i] || min == myscore[i]){
+
+                average[i] = (average[i] - myscore[i])/(size - 1);
+
+            }
+            else{
+                average[i] = average[i]/size;
+            }
+            max = 0;
+            min = 100;
+            flag = false;
         }
 
-        //max, min을 통해 자기자신이 평가한 점수가 max 또는 min이면 빼고 평균을 구한다.
-        for(int j = 0; j < size; j++) {
-            max = myscore[j];
-            min = myscore[j];
-            for (int i = 0; i < size; i++) {
-                if (max < scores[i][j])
-                    max = scores[i][j];
-                if (min > scores[i][j])
-                    min = scores[i][j];
-            }
-
-            //max와 min이 자기자신의 점수와 같은 경우 유일한 최저점 또는 최고점 인지 판단한 후 제거한다.
-            if (max == myscore[j] || min == myscore[j]){
-                if(myscore[j] == arrayList.get(j).get(size-2) || myscore[j] == arrayList.get(j).get(1)){
-                }
-                else{
-
-                    arrayList.get(j).remove(arrayList.get(j).indexOf(myscore[j]));
-                }
-
-            }
-
-            for (int i : arrayList.get(j)) {
-                average[j] += i;
-            }
-            average[j] = average[j] / arrayList.get(j).size();
-        }
-
-        //각각 점수에 대해서 학점을 매겨준다.
         for(float i: average){
             if(90 <= i)
                 answer += "A";
@@ -69,11 +50,9 @@ class Solution {
             else if(i < 50)
                 answer += "F";
         }
-
         return answer;
     }
 }
-
 
 
 
