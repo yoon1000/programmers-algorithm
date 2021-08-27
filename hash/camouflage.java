672a67e78b6d27253ff3ccee88f41ba9a93f9b3a
yoon1000 <lucayang@naver.com>
2021-08-28 01:05:25 +0900

    Update camouflage.java/ Solved

--- hash/camouflage.java
+++ hash/camouflage.java
@@ -4,60 +4,17 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-       int answer = 0; int length = clothes.length; int result = 0;
-        ArrayList<String> list = new ArrayList<>();
-        HashMap<String, String> hashMap = new HashMap<>();
+        int answer = 1;
+        int length = clothes.length;
+        HashMap<String, Integer> hashMap = new HashMap<>();
         for(int i = 0; i < length; i++){
-            hashMap.put(clothes[i][0], clothes[i][1]);
+            hashMap.put(clothes[i][1], hashMap.getOrDefault(clothes[i][1], 0)+1);
         }
-
-        String arr[] = new String[clothes.length];
-        for(int i =0; i< clothes.length; i++){
-            arr[i] = clothes[i][0];
+        for(int val: hashMap.values()){
+            answer = answer * (val + 1);
         }
-        boolean visited[] = new boolean[clothes.length];
-        combination(arr, visited, 0, 3, 2, hashMap);
+        answer = answer -1;
 
         return answer;
-    }
-  
-   static void combination(String[] arr, boolean[] visited, int start, int n, int r, HashMap<String, String> hashMap) {
-        ArrayList<String> temp = new ArrayList<>();
-        HashMap<String, String> listMap = new HashMap<>();
-        int count = 0;
-        if (r == 0) {
-            temp = print(arr, visited, n);
-            for(String a : temp){
-                listMap.put(a, hashMap.get(a));
-            }
-            listMap.remove(temp.get(0));
-            if(listMap.containsValue(hashMap.get(temp.get(0))))
-                System.out.println("sameclothes...");
-            else
-                System.out.println("not same///");
-            /*for(String a:temp){
-                System.out.print(a +" ");
-            }
-            System.out.println();*/
-            return;
-        }
-
-        for (int i = start; i < n; i++) {
-            visited[i] = true;
-            combination(arr, visited, i + 1, n, r - 1, hashMap);
-            visited[i] = false;
-        }
-    }
-  
-  static ArrayList<String> print(String[] arr, boolean[] visited, int n) {
-        ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (visited[i]) {
-                list.add(arr[i]);
-                //System.out.print(arr[i] + " ");
-            }
-        }
-        //System.out.println();
-        return list;
     }
 }
