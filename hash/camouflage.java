98c4cd6cea96a73f2c9fc8e714748de245a926ad
yoon1000 <lucayang@naver.com>
2021-08-28 21:16:02 +0900

    add annotation

--- hash/camouflage.java
+++ hash/camouflage.java
@@ -6,13 +6,16 @@ class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
         int length = clothes.length;
+        // 옷의 종류별로 몇 벌인지 hashmap으로 구분한다.
         HashMap<String, Integer> hashMap = new HashMap<>();
         for(int i = 0; i < length; i++){
             hashMap.put(clothes[i][1], hashMap.getOrDefault(clothes[i][1], 0)+1);
         }
+        // 옷 종류당 안 입는 경우를 포함하여 모든 경우의 수를 곱해준다.
         for(int val: hashMap.values()){
             answer = answer * (val + 1);
         }
+        // 모두 안 입는 경우를 빼준다.
         answer = answer -1;
 
         return answer;
--- hash/phoneNumList.java
+++ hash/phoneNumList.java
@@ -1,14 +1,17 @@
-// 코딩테스트 고득점 Kit - Hash - 전화번호 목록
+// level 2 - 코딩테스트 고득점 Kit - Hash - 전화번호 목록
 
 import java.util.*;
+
 class Solution {
     public boolean solution(String[] phone_book) {
-         boolean answer = true;
+        boolean answer = true;
+        // 각 전화번호와 길이를 hashmap에 저장한다.
         HashMap<String, Integer> length = new HashMap<>();
         for(int i = 0; i < phone_book.length; i++){
             length.put(phone_book[i], phone_book[i].length());
         }
 
+        // 전화번호의 substring한 것이(접두사로) hashmap에 포함되어 있는지 판단한다.
         for(int i = 0; i < phone_book.length; i++){
             for(int j = 1; j < phone_book[i].length(); j++){
                 if(length.containsKey(phone_book[i].substring(0,j))){
